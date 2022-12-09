3cdc3a32b8793f94ac1f2ecf817a9fc0a9f51baf
KingaGawron <64367504+KingaGawron@users.noreply.github.com>
2022-12-09 01:33:09 +0100

    arrow

--- Lab2/main.js
+++ Lab2/main.js
@@ -6,5 +6,22 @@ setInterval(function(){
     if(counter > 4){
         counter = 1
     }
-
 },5000);
+
+const arrowRight = document.querySelector("right");
+const arrowLeft = document.querySelector("left");
+const img = document.querySelector("slide");
+let currentImgIndex;
+img.forEach((slide,index) => {
+    slide.addEventListener("click",() =>{
+
+        currentImgIndex=index;
+        alert(currentImgIndex);
+    });
+    
+});
+
+arrowRight.addEventListener("click",()=>{
+    counter++;
+}
+)
