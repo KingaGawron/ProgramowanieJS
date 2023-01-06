d7f3b9916ee32b5ef5711ec5eb337e3672b47664
KingaGawron <64367504+KingaGawron@users.noreply.github.com>
2023-01-06 14:07:00 +0100

    lab7

---
+++ Lab7/main.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("canvas");
+var window_height=window.innerHeight;
+var window_width=window.innerWidth;
+let context = canvas.getContext("2d");
+
+
+canvas.height=window_height;
+canvas.width=window_width;
+
+canvas.style.background="#ff8";
