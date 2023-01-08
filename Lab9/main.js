6bb4f6f1b6886431bd46a8c3ea2a73c9a221ad3a
KingaGawron <64367504+KingaGawron@users.noreply.github.com>
2023-01-08 19:34:37 +0100

    done

--- Lab9/main.js
+++ Lab9/main.js
@@ -1,5 +1,7 @@
 let weather = {
   apiKey: "cb263a9ed6988851a2ee09f0fe224e91",
+  weatherArray:[],
+  
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -10,28 +12,53 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
+  
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     console.log(name, icon, description, temp, humidity);
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "http://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + " °C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-      document.querySelector(".weather").classList.remove("loading");
-  },
-  search:function(){
-    this.fetchWeather(document.querySelector(".search-bar").value);
+    let divTag = ` <div id="${name.toLowerCase()}" class="weather loading">
+    <h1 class="city">Weather in ${name}</h1>
+    <h2 class="temp">${temp} °C </h2>
+    <div class="flex">
+      <img src="http://openweathermap.org/img/wn/${icon}.png" alt="" class="icon" />
+      
+      <p class="description">${description}</p>
+    </div>
+    <p class="humidity">Humidity: ${humidity}% </p>
+  </div> `;
+  if(this.weatherArray.length>9){
+    const deleted = this.weatherArray.shift();
+    document.querySelector("#"+deleted).remove();
+    
   }
+    document.querySelector("#weathers").innerHTML+=divTag
+    this.weatherArray.push(name.toLowerCase());
+
+    localStorage.setItem("weathers", JSON.stringify(this.weatherArray));
+   
+
+  },
+  search: function () {
+    const searchBar=document.querySelector(".search-bar").value;
+    if (this.weatherArray.includes(searchBar)) {
+      alert("To miasto już było")
+
+    } else {
+      this.fetchWeather(searchBar);
+    }
+  },
 };
-document.querySelector(".search button").addEventListener("click",function(){
-    weather.search();
-})
-document.querySelector(".search-bar").addEventListener("keyup",function(event){
-    if(event.key=="Enter")
-    weather.search();
-})
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") weather.search();
+  });
+  const zmienna = JSON.parse(localStorage.getItem("weathers") || "[]")
+  zmienna.forEach(element => {
+    weather.fetchWeather(element);
+  });
